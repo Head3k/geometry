a79286cc269d21761cf78dde06242f345b0d72f8
Nickola_Fedora <Nickolablaniy@mail.ru>
2020-05-08 16:58:18 +0700

    Local libruary

---
+++ src/geometry.h
@@ -0,0 +1,8 @@
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int function(float x1, float y1, float r1, float x2, float y2, float r2);
+float circle(float x, float y, float r, float* p, float* s);
